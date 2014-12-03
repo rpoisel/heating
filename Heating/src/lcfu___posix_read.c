04aeb41fe7597f9ebbe6cd64dc234bb88c5536f3
Rainer Poisel <rainer.poisel@gmail.com>
2014-12-03 19:57:57 +0000

    Analog inputs working

---
+++ Heating/src/lcfu___posix_read.c
@@ -0,0 +1,30 @@
+#ifndef LC_PROT_LCFU___POSIX_READ__C
+#define LC_PROT_LCFU___POSIX_READ__C
+
+#include <lcfu___posix_read.h>
+#include <unistd.h>
+
+/*                            Functions                        */
+void  lcfu___POSIX_READ(LC_TD_Function_POSIX_READ* LC_this, LC_TD_DINT LC_VD_FD, LC_TD_DINT LC_VD_COUNT, struct _lcoplck_epdb_1_impl* pEPDB)
+{
+	/*
+	   LC_TD_BOOL LC_VD_ENO;
+  	   LC_TD_DINT LC_VD_ERRNO;
+       LC_TD_LINT LC_VD_POSIX_READ;
+       LC_TD_BYTE LC_VD_DATA[64];
+	 */
+  int cnt = 0;
+
+  LC_this->LC_VD_POSIX_READ = read(LC_VD_FD, LC_this->LC_VD_DATA, LC_VD_COUNT);
+  if (LC_this->LC_VD_POSIX_READ == LC_VD_COUNT)
+  {
+	for (cnt = 0; cnt < LC_VD_COUNT; cnt++)
+	{
+		printf("0x%02x ", LC_this->LC_VD_DATA[cnt]);
+	}
+	printf("\n");
+  }
+  LC_this->LC_VD_ENO = LC_EL_true;
+}
+
+#endif
---
+++ Heating/src/lcfu___posix_write.c
@@ -0,0 +1,26 @@
+#ifndef LC_PROT_LCFU___POSIX_WRITE__C
+#define LC_PROT_LCFU___POSIX_WRITE__C
+
+#include <lcfu___posix_write.h>
+#include <unistd.h>
+
+/*                            Functions                        */
+void  lcfu___POSIX_WRITE(LC_TD_Function_POSIX_WRITE* LC_this, LC_TD_DINT LC_VD_FD, LC_TD_DINT LC_VD_COUNT, LC_TD_BYTE LC_VD_DATA[64], struct _lcoplck_epdb_1_impl* pEPDB)
+{
+  /* LC_TD_BOOL LC_VD_ENO;
+     LC_TD_DINT LC_VD_ERRNO;
+     LC_TD_LINT LC_VD_POSIX_WRITE;
+   */
+	int cnt = 0;
+
+	for (cnt = 0; cnt < LC_VD_COUNT; cnt++)
+	{
+		printf("0x%02x ", LC_VD_DATA[cnt]);
+	}
+	printf("\n");
+
+	LC_this->LC_VD_POSIX_WRITE = write(LC_VD_FD, LC_VD_DATA, LC_VD_COUNT);
+	LC_this->LC_VD_ENO;
+}
+
+#endif
